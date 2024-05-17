f3582309d4b06090a8250f26738c975a73cd2b66
Lyubawa <lyubawa81@ukr.net>
2024-05-17 08:30:46 +0300

    hw-node-4

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
+import usersRouter from "./routes/authRouter.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 const DB_URI = process.env.DB_URI;
 
@@ -21,7 +23,9 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", authMiddleware, contactsRouter);
+app.use("/api/auth", usersRouter);
+app.use("/api/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
